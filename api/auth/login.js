b49249ef1ad27912297265afa6e75f5e90fd3a5c
kaichong lin <coco.keven@gmail.com>
2024-05-15 20:44:48 -0500

    login router updated.

--- api/auth/login.js
+++ api/auth/login.js
@@ -21,20 +21,22 @@ router.post("/",authenticateByUsernamePassword, async (req, res, next) => {
         username: username,
       },
     });
+
+    resMap.set("token",currentToken)
+
     if(!currentToken){
         const token = generateToken(user)
         resMap.set("token",token)
-    }else if (isExpired(currentToken) && verifyToken(currentToken)) {
+    }else if(isExpired(currentToken)) {
         const refreshToken = generateRefreshToken(user);
         resMap.set("token",refreshToken)
     }
 
     resMap.set("user",user);
-    console.log("resMap:",resMap)
 
     return res.status(200).json(Object.fromEntries(resMap))
   } catch (e) {
-    res.status(400).send("Unauthorized");
+    res.status(400).send(`Unauthorized:${e.message}`);
     next(e);
   }
 });
